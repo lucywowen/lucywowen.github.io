3e52f7e7cdcff1e3618039a463d0b4d68a770309
lucy <lucywowen@gmail.com>
2018-09-10 11:27:05 -0400

    update contact page

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,12 +14,12 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 
 // create email body and send it
-$to = 'andrew.heusser@gmail.com'; // put your email address here
+$to = 'lucywowen@gmail.com'; // put your email address here
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message: \n $message";
-$headers = "From: andrew.heusser@gmail.com\n"; 
+$headers = "From: andrew.heusser@gmail.com\n";
 // Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
